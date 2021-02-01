2314ea6d2475e8374860f5f99a965cb955e37286
mukesh22584 <mks22584@gmail.com>
2021-02-01 16:41:07 +0530

    sm8150-common: build vendor.display.config@1.0.vendor
    library "vendor.display.config@1.0.so" not found

--- common.mk
+++ common.mk
@@ -253,6 +253,7 @@ PRODUCT_PACKAGES += \
     libtinyxml \
     libvulkan \
     memtrack.msmnile \
+    vendor.display.config@1.0.vendor \
     vendor.display.config@2.0 \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.allocator@1.0-service \
