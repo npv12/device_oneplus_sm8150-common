e1d3c5242feb894743813f92fa37274cb3e364f2
Pranav <npv12@iitbbs.ac.in>
2021-10-11 23:41:01 +0530

    sm8150-common: build android.hardware.secure_element1.2 from source as well
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- common.mk
+++ common.mk
@@ -377,6 +377,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
     android.hardware.secure_element@1.1.vendor \
+    android.hardware.secure_element@1.2.vendor \
     libprotobuf-cpp-full \
     libril_shim \
     librmnetctl \
