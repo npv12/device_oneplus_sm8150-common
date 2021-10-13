997129271f551b65b222abfccfe24819e7872f26
Pranav <npv12@iitbbs.ac.in>
2021-10-13 10:24:54 +0530

    sm8150-common: drop qti power hal
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- common.mk
+++ common.mk
@@ -362,7 +362,6 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
     android.hardware.power@1.2.vendor \
     vendor.qti.hardware.perf@2.2.vendor:64
 
